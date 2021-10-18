3f4724e2a5f90c50ba9de84e77a931fc2e9b43c8
talschreiber95 <talfreestyle@gmail.com>
2021-10-18 19:03:29 +0300

    javascript course was finish 18.10.21

---
+++ JavaScript/main.js
@@ -0,0 +1,13 @@
+//was fallowed by this video https://www.youtube.com/watch?v=hdI2bqOjy3c&t=191s
+// variable: let , const
+let x=3;
+x=4;
+console.log(x); // x=4
+const y=3;
+// y=2; can't reassign this variable by another value
+console.log(y) // y=3
+// datatypes
+//String,Numbers,Boolean,null,undefined,Symbol
+const name = 'Tal';
+const age = 26;
+const ar= [1,5,3,4];
